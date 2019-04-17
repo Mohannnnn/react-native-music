6a85834ef4d424125f14e0e173e7bd7175605ab7
Mohan <1063022109@qq.com>
2019-04-17 14:55:40 +0800

    修改input组件

--- src/components/common/input.js
+++ src/components/common/input.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput , StyleSheet, Text, View } from "react-native";
+import { TextInput , StyleSheet, Text, View , Image } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class Input extends React.Component{
@@ -10,13 +10,13 @@ class Input extends React.Component{
         }
     }
     render(){
-        console.log(this.props)
         return (
             <View style={styles.container}>
+                <Image style={styles.image} source={require('../../static/images/search.png')}/>
                 {/* <Ionicons name={'ios-search'} size={15} color='#d3d3d4'/>, */}
                 <TextInput style={styles.input}
                 onChangeText={(txt) => this.props.onChangeText(txt)}
-                defaultValue={this.props.defaultValue}
+                placeholder={this.props.placeholder}
                 />
             </View>
         )
@@ -29,6 +29,13 @@ var styles = StyleSheet.create({
         alignItems:'center',
         backgroundColor:'transparent',
     },
+    image : {
+        position:'absolute',
+        left : 10,
+        zIndex:5,
+        width:15,
+        height:15
+    },
     input: {
         borderColor: "#f7f7f7",
         backgroundColor: "#f7f7f7",
@@ -36,11 +43,11 @@ var styles = StyleSheet.create({
         padding: 0,
         width:250,
         height:35,
-        paddingLeft: 10,
+        paddingLeft: 30,
         paddingRight: 10,
         color:'#d3d3d4',
         fontSize: 15,
-        textAlign:'center'
+        textAlign:'left'
     }
 })
 
